edf8c5ddf6b4da36392e673c73018bfef1ef36f1
Kohei56 <kohei712@gmail.com>
2018-10-18 17:36:16 +0900

    CORS設定の追加

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 const model = require('./model');
 const nemdb = model.nemdb;
 
+// CORSを許可する
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // bodyParserの設定
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
